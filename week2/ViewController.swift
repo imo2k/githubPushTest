929612655e7fff784dd3e596e28bec25a779602b
imo2k <dnrwk99@gmail.com>
2024-12-30 13:23:02 +0900

    Initial Commit

---
+++ week2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week2
+//
+//  Created by GO on 12/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
